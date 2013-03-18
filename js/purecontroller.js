87ef2d8e395ddc94d04f0285eb054e0a8f1f15a8
Grad van Horck <git@gradje.nl>
2013-03-18 21:41:54 +0100

    Some improvements.

--- js/purecontroller.js
+++ js/purecontroller.js
@@ -16,29 +16,18 @@ var PureController = {
         acl23: 13,
         acl24: 14,
 
-        led11R: 201,
-        led11G: 202,
-        led11B: 203,
-        led12R: 204,
-        led12G: 205,
-        led13R: 206,
-        led13R: 207,
-        led13G: 208,
-        led13B: 209,
+        led11: 201,
+        led12: 208,
+        led13: 209,
 
-        led21R: 211,
-        led21G: 212,
-        led21B: 213,
-        led22R: 214,
-        led22G: 215,
-        led23R: 216,
-        led23R: 217,
-        led23G: 218,
-        led23B: 219
+        led21: 211,
+        led22: 218,
+        led23: 219
     },
 
     outputs: new Array(512),
     bindings: {},
+    bindingsRgb: {},
 
     init: function() {
 
@@ -63,8 +52,15 @@ var PureController = {
 
         $('input[type=range]').on('change', function() {
             var id = this.id;
-            PureController.setValue(id, this.value);
-            PureController.updatePreview();
+            if (id == 'led_r') {
+                var values = [this.value, false, false];
+            } else if (id == 'led_g') {
+                var values = [false, this.value, false];
+            } else if (id == 'led_b') {
+                var values = [false, false, this.value];
+            }
+            PureController.setValue('led11,led12,led13,led21,led22,led23', values);
+
         });
 
         $('button[data-type=ledpreset]').on('click', function() {
@@ -82,7 +78,17 @@ var PureController = {
                 PureController.bindings[key].push(this);
             }
 
-        })
+        });
+
+        $('*[data-type=output-rgb]').each(function() {
+            var key = $(this).data('bind');
+            if (!PureController.bindingsRgb[key]) {
+                PureController.bindingsRgb[key] = [ this ];
+            } else {
+                PureController.bindingsRgb[key].push(this);
+            }
+
+        });
 
         for(var key in PureController.config) {
 
@@ -90,13 +96,6 @@ var PureController = {
 
         }
 
-    },
-
-    updatePreview: function() {
-        var r = parseInt($('input[type=range]#led_r').val()),
-            g = parseInt($('input[type=range]#led_g').val()),
-            b = parseInt($('input[type=range]#led_b').val())
-        $('#rgb-preview div').css('background-color', 'rgb('+r+','+g+','+b+')');
     },
 
     setValue: function(channelName, value) {
@@ -109,13 +108,30 @@ var PureController = {
         }
 
         var channel = PureController.config[channelName];
-        PureController.outputs[channel] = value;
+        if (typeof value === 'array') {
+            for(var i=0;i<value.length;i++ ){
+                if (value[i] !== false) {
+                    PureController.outputs[channels+i] = value[i];
+                }
+            }
+        } else {
+            PureController.outputs[channel] = value;
+        }
 
         if (PureController.bindings[channelName]) {
             for(var i=0;i<PureController.bindings[channelName].length;i++) {
                 var element = PureController.bindings[channelName][i];
                 element.style.backgroundColor = 'rgb('+value+','+value+','+value+')';
             };
+        } else if(PureController.bindingsRgb[channelName]) {
+
+            for(var i=0;i<PureController.bindingsRgb[channelName].length;i++) {
+                var element = PureController.bindingsRgb[channelName][i];
+
+                element.style.backgroundColor = 'rgb('+value[0]+','+value[1]+','+value[2]+')';
+            };
+
+
         } else {
             console.warn('control '+channelName+' not found');
         }
@@ -132,10 +148,7 @@ var PureController = {
         var interval = 30, step = 0;
 
         if (time === 0) {
-            $('input[type=range]#led_r').val(values[0]);
-            $('input[type=range]#led_g').val(values[1]);
-            $('input[type=range]#led_b').val(values[2]);
-            $('input[type=range]').trigger('change');
+            PureController.setValue('led11,led12,led13,led21,led22,led23', values);
             return;
         }
 
@@ -149,10 +162,14 @@ var PureController = {
 
         var animFrame = function() {
             console.log(from[0], values[0], (((values[0] - from[0]) / steps) * step), from[0] + (((values[0] - from[0]) / steps) * step), step);
-            $('input[type=range]#led_r').val(from[0] + (((values[0] - from[0]) / steps) * step));
-            $('input[type=range]#led_g').val(from[1] + (((values[1] - from[1]) / steps) * step));
-            $('input[type=range]#led_b').val(from[2] + (((values[2] - from[2]) / steps) * step));
-            $('input[type=range]').trigger('change');
+            var newValues = [(from[0] + (((values[0] - from[0]) / steps) * step)),
+                             (from[1] + (((values[1] - from[1]) / steps) * step)),
+                             (from[2] + (((values[2] - from[2]) / steps) * step))];
+            $('input[type=range]#led_r').val(newValues[0]);
+            $('input[type=range]#led_g').val(newValues[1]);
+            $('input[type=range]#led_b').val(newValues[2]);
+            PureController.setValue('led11,led12,led13,led21,led22,led23', newValues);
+            //$('input[type=range]').trigger('change');
 
             step++;
 
