b801a7cc577653655609aa72ead12c44de064d53
Grad van Horck <git@gradje.nl>
2013-03-19 23:24:45 +0100

    Started work on rewrite of RGB controller

--- js/purecontroller.js
+++ js/purecontroller.js
@@ -10,11 +10,12 @@ PureController.prototype.init = function() {
     console.log('Initializing');
     console.log(this.config);
 
-    this.bindButtons();
-    this.bindOutputs();
+    this.bindDimmerButtons();
+    this.bindDimmerOutputs();
+    this.bindRgbPresetButtons();
 };
 
-PureController.prototype.bindButtons = function() {
+PureController.prototype.bindDimmerButtons = function() {
 
     var self = this;
 
@@ -52,7 +53,28 @@ PureController.prototype.bindButtons = function() {
 
 };
 
-PureController.prototype.bindOutputs = function() {
+PureController.prototype.bindRgbPresetButtons = function() {
+    var self = this;
+
+    var buttons = document.querySelectorAll('[data-type=ledpreset]');
+
+    for(var i=0;i<buttons.length;i++) {
+
+        var button = buttons[i];
+        button.addEventListener('click', function(e) {
+
+            var button = e.target;
+            var value = button.getAttribute('data-value')
+            var fadeTime = button.getAttribute('data-fade');
+
+            self.setRgbPreset(value);
+
+        });
+
+    }
+}
+
+PureController.prototype.bindDimmerOutputs = function() {
 
     var self = this;
     var outputs = document.querySelectorAll('[data-type=output]');
@@ -100,8 +122,45 @@ PureController.prototype.setDimmerValueByName = function(channelName, newValue) 
         }
 
     }
-}
+};
 
+PureController.prototype.setRgbPreset = function(value) {
+    var values = value.split(',');
+    console.log(values);
+    if (values.length = 1) {
+        values = [value, value, value];
+    }
+    while(values.length < 3) {
+        values.push(values[0]);
+    }
+    if (values.length <= 3) {
+        // all six same
+        for(i=0;i<5;i++) {
+            values.push(values[0]);
+            values.push(values[1]);
+            values.push(values[2]);
+        }
+    }
+    while(values.length < 6) {
+        values.push(values[0]);
+    }
+    if (values.length <= 6) {
+        // 0 1 2
+        values.splice(3, 6, values[0], values[1], values[2], values[0], values[1], values[2]);
+        // 3 4 5
+        // 6 7 8
+
+        // 9 10 11
+        values.push(values[9]); values.push(values[10]); values.push(values[11]);
+        values.push(values[9]); values.push(values[10]); values.push(values[11]);
+    }
+
+    var groupName = 'leds';
+    var groupNames = this.config.groups[groupName];
+
+    console.log(values);
+
+};
 
 var PureDummyInterface = function() {
 
